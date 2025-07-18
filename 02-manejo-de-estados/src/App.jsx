582c6c30d988e065d4dfcac9286fcbaf8148adae
J0n4s4n <jonh.reyes.go@gmail.com>
2025-07-18 15:22:19 -0600

    feat: :rocket: Finishing project 2
    In this commit I upload the project 2 (a list that renders in realtime with react and hooks) It contains all the features requested ion campus

---
+++ 02-manejo-de-estados/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import ListaCompras from './components/ListaDeCompras'
+
+function App() {
+
+  return (
+    <>
+      <ListaCompras></ListaCompras>
+    </>
+  )
+}
+
+export default App
