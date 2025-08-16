c2cfca6ba46641c0820c7d8fe4b749e1bfa12652
J0n4s4n <jonh.reyes.go@gmail.com>
2025-08-16 16:28:56 -0600

    Refactor project to integrate Tailwind CSS and remove unused assets
    - Updated package.json to include Tailwind CSS and removed MUI dependencies.
    - Deleted unused SVG assets (vite.svg and react.svg) from the public and src/assets directories.
    - Removed App.css and replaced styles with Tailwind CSS utility classes.
    - Modified Game component to apply Tailwind CSS classes for styling.
    - Updated index.css to import Tailwind CSS.
    - Adjusted vite.config.js to include Tailwind CSS plugin.
    - Added new Icon.png asset to the public directory.

--- 05-conditional-rendering/02-adivina-el-numero/src/components/Game.jsx
+++ 05-conditional-rendering/02-adivina-el-numero/src/components/Game.jsx
@@ -36,10 +36,11 @@ export const Game = () => {
   }, [answer, randomInt])
 
   return (
-    <>
+    <div className='text-amber-50 absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]'>
       <div>{randomInt}</div>
       <p>{status}</p>
       <input onChange={inputHandler} type='number' min='0' name='' id='' />
-    </>
+    </div>
+
   )
 }
