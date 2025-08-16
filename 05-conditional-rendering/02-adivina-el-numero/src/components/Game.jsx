5f4dcf841798dd607166276abfc512a1d3d2e8ac
J0n4s4n <jonh.reyes.go@gmail.com>
2025-08-16 15:59:33 -0600

    feat: ✨ implement number guessing game with conditional rendering
    - Added dependencies for Material UI and Emotion for styling.
    - Refactored App component to render Game component.
    - Created Game component to handle random number generation and user input.
    - Implemented state management for random number, user answer, and status messages.
    - Added input field for user guesses and conditional rendering for feedback.

--- 05-conditional-rendering/02-adivina-el-numero/src/components/Game.jsx
+++ 05-conditional-rendering/02-adivina-el-numero/src/components/Game.jsx
@@ -1,7 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
+const generateRandomInt = (multFactor) => {
+  return Math.floor(Math.random() * multFactor)
+}
+
+// En el componente Game, genera un número aleatorio entre 1 y 100 cuando el juego comienza.
+// Guarda el estado del número ingresado y la respuesta en useState.
+// Compara el número ingresado con el generado y usa renderización condicional para mostrar:
+//     "¡Correcto!" si el usuario acierta.
+//     "El número es mayor" o "El número es menor" como pistas.
 
 export const Game = () => {
+  const [randomInt, setRandomInt] = useState(0)
+  const [answer, setAnswer] = useState(0)
+  const [status, setStatus] = useState('hola')
+
+  const inputHandler = (event) => {
+    setAnswer(event.target.value)
+  }
+
+  useEffect(() => {
+    setRandomInt(generateRandomInt(100))
+  }, [])
+
+  useEffect(() => {
+    if (answer < randomInt) {
+      setStatus('frio')
+    }
+    if (answer > randomInt) {
+      setStatus('caliente')
+    }
+    if (parseInt(answer) === parseInt(randomInt)) {
+      setStatus('ganate')
+    }
+  }, [answer, randomInt])
+
   return (
-    <div>Game</div>
+    <>
+      <div>{randomInt}</div>
+      <p>{status}</p>
+      <input onChange={inputHandler} type='number' min='0' name='' id='' />
+    </>
   )
 }
