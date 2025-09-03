41efe4d38d356716cabfc11a6ab1d3b9b5ab5bd6
J0n4s4n <jonh.reyes.go@gmail.com>
2025-09-03 15:45:48 -0600

    feat: :sparkles: Functional demo form
    A form that uses state and captures the data.

--- 06-hooks-avanzados-react/01-practica-clase/src/components/FormComponent.jsx
+++ 06-hooks-avanzados-react/01-practica-clase/src/components/FormComponent.jsx
@@ -8,33 +8,74 @@ export const FormComponent = () => {
       password:''
   })
 
+  // Destructuring the state objects
+  const {userName, userEmail, password} = formData
+
+  // Funtion that manages the input on change event
+  const inputChange = ({target}) => {
+    // Destructuring from target the two elements that we will use
+    const {name, value} = target
+    setFormData({
+      // First REMEMBER to make a copy of the elemets with the spread operator
+      ...formData,
+      // ? Propiedad computada
+      // Using [] will tell JS that is variable, so any name will be ok form the lists of imputs
+      [name]: value
+
+    })
+  }
+
   return (
+    // ? ==+ Class notes +==
+    // When importing html elements, it's impotant to take on mind the reserved JS words
+    // class => className, for => htmlFor and the closing tags (on form elements)
     <form>
       <div className="mb-3">
-        <label htmlFor="exampleInputEmail1" className="form-label">
+        <label htmlFor="userName" className="form-label">
+          Name
+        </label>
+        <input
+          type="text"
+          className="form-control"
+          name="userName"
+          aria-describedby="emailHelp"
+          // Value has to point to the useState
+          value={userName}
+          onChange={ inputChange }
+        />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="userEmail" className="form-label">
           Email address
         </label>
         <input
           type="email"
           className="form-control"
-          id="exampleInputEmail1"
+          name="userEmail"
           aria-describedby="emailHelp"
+          value={userEmail}
+          onChange={ inputChange }
         />
-        <div id="emailHelp" className="form-text">
+        <div name="emailHelp" className="form-text">
           We'll EVER share your email with anyone else MUAJAJAJJA.
         </div>
       </div>
       <div className="mb-3">
-        <label htmlFor="exampleInputPassword1" className="form-label">
+        <label htmlFor="password" className="form-label">
           Password
         </label>
         <input
           type="password"
           className="form-control"
-          id="exampleInputPassword1"
+          name="password"
+          value={password}
+          onChange={ inputChange }
         />
       </div>
-      <button type="submit" className="btn btn-primary">
+      <button 
+        type="submit" 
+        className="btn btn-primary"
+        >
         Submit
       </button>
     </form>
