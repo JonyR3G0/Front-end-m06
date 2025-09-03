69dce09405a50011622f5d64c3dbf37f074f503c
J0n4s4n <jonh.reyes.go@gmail.com>
2025-09-02 19:20:29 -0600

    style: :bulb: Documenting the Input number component
    Adding  JS docs comments and comments to the inputNumber component

--- 05-conditional-rendering/02-adivina-el-numero/src/components/InputNumber.jsx
+++ 05-conditional-rendering/02-adivina-el-numero/src/components/InputNumber.jsx
@@ -1,7 +1,12 @@
 import { useContext } from 'react'
 import { InputUserContext } from '../context/InputContext'
 
+/**
+ * This component uses the global context to actualize and consume the user input state
+ * @returns The visual component for the input of the user
+ */
 export const InputNumber = () => {
+  // Importing the context that manages the state of the userInput across all components
   const { inputContextState, setInputContextState } = useContext(InputUserContext)
   return (
     <div>
