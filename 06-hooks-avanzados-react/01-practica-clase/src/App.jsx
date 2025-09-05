08aa2c05c41e731b75068586c4b6b83eb0e638b3
J0n4s4n <jonh.reyes.go@gmail.com>
2025-09-04 20:19:53 -0600

    feat: :sparkles: UseCallback demo 2
    Uploading an example of UseCallback

--- 06-hooks-avanzados-react/01-practica-clase/src/App.jsx
+++ 06-hooks-avanzados-react/01-practica-clase/src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { FormComponentUseRef } from './components/FormComponentUseRef.jsx'
 import { ContadorConCallback } from "./components/CounterWithCallback";
+import { Padre } from "./components/Padre.jsx";
 
 export const App = () => {
   return (
     <>
-    <ContadorConCallback/>
+      <Padre/>
     </>
   )
 }
