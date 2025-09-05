a4bf88359ad4be0921a04a472730b10ab732f56e
J0n4s4n <jonh.reyes.go@gmail.com>
2025-09-04 23:33:33 -0600

    feat: :sparkles: Demo 2 useReducer form

--- 06-hooks-avanzados-react/01-practica-clase/src/App.jsx
+++ 06-hooks-avanzados-react/01-practica-clase/src/App.jsx
@@ -3,11 +3,12 @@ import { FormComponentUseRef } from './components/FormComponentUseRef.jsx'
 import { ContadorConCallback } from "./components/CounterWithCallback";
 import { Padre } from "./components/Padre.jsx";
 import { Counter } from './components/Counter.jsx';
+import { UseReducerForm } from './components/UseReducerForm.jsx';
 
 export const App = () => {
   return (
     <>
-      <Counter/>
+      <UseReducerForm/>
     </>
   )
 }
