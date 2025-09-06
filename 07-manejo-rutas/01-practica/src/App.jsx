26357933b82b823048181957de777fe054782bdc
J0n4s4n <jonh.reyes.go@gmail.com>
2025-09-06 17:05:26 -0600

    feat: :sparkles: Implementing the router function
    To 3 different pages

--- 07-manejo-rutas/01-practica/src/App.jsx
+++ 07-manejo-rutas/01-practica/src/App.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import { NavBar } from './components/NavBar'
+import React from "react";
+import { NavBar } from "./components/NavBar";
+import { Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { About } from "./pages/About";
+import { Store } from "./pages/Store";
 
 export const App = () => {
   return (
     <div>
-      <NavBar></NavBar>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about-us" element={<About />} />
+        <Route path="/aperture-store-cake" element={<Store />} />
+      </Routes>
     </div>
-  )
-}
+  );
+};
