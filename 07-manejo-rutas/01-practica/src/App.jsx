2b010163f50b2b77874dfc1bba0924f26fd2e26c
J0n4s4n <jonh.reyes.go@gmail.com>
2025-09-06 16:38:11 -0600

    feat: :sparkles: Importing bootstrap CDN and navbar component.

--- 07-manejo-rutas/01-practica/src/App.jsx
+++ 07-manejo-rutas/01-practica/src/App.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import { NavBar } from './components/NavBar'
 
 export const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <NavBar></NavBar>
+    </div>
   )
 }
