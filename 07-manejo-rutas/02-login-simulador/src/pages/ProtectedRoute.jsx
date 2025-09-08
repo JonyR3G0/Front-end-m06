0422d139a2e63652505497f156d01f64fcc98e2b
J0n4s4n <jonh.reyes.go@gmail.com>
2025-09-07 20:54:02 -0600

    feat: :sparkles: Protected routes component
    Adding a component that only returns children components if authenticated based on the AuthContext

---
+++ 07-manejo-rutas/02-login-simulador/src/pages/ProtectedRoute.jsx
@@ -0,0 +1,9 @@
+import { useAuth } from "../context/AuthContext";
+import { Navigate } from "react-router-dom";
+
+export const ProtectedRoute = ({children}) => {
+  
+    const {isAuth} = useAuth()
+
+    return isAuth ? children : <Navigate to='/login'/>
+}
