9239e5082ddf308b479d09b28f4cce1bdeef5eb8
J0n4s4n <jonh.reyes.go@gmail.com>
2025-09-11 20:37:13 -0600

    feat: :sparkles: Adding TweetList component

---
+++ 08-proyecto-final-intro-react/mi-twitter-clon/src/components/TweetList.jsx
@@ -0,0 +1,13 @@
+import { 
+    Tweet
+ } from "./Tweet";
+
+
+export const TweetList = () => {
+  return (
+    <div>
+        {/* Needs a map function */}
+        <Tweet tweet={} onStar={}/>
+    </div>
+  )
+}
