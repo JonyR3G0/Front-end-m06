b01405373a80ab50e4001f541a7f7de3f236754a
J0n4s4n <jonh.reyes.go@gmail.com>
2025-09-13 03:44:22 -0600

    feat: :sparkles: Not Found page
    Adding a not found page

--- 08-proyecto-final-intro-react/mi-twitter-clon/src/pages/NotFound.jsx
+++ 08-proyecto-final-intro-react/mi-twitter-clon/src/pages/NotFound.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
 
+const containerStyle =
+  'bg-black h-dvh w-dvw text-gray-500 flex flex-col text-center items-center'
+const buttonStyle =
+  'bg-sky-500 w-min rounded-4xl text-white font-bold text-md p-1 pl-4 pr-4 hover:cursor-pointer hover:bg-sky-600 transition-all duration-300 m-5'
+const container = 'm-20'
+
 export const NotFound = () => {
   return (
-    <div>NotFoun</div>
+    <div className={containerStyle}>
+      <div className={container}>
+        <p>Hmm... esta pagina no existe. Intenta buscar otra cosa. (ᵕ—ᴗ—)</p>
+        <button className={buttonStyle}>Buscar</button>
+      </div>
+    </div>
   )
 }
